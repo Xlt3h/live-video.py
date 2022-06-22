2a02ad0abb8a150c8f02b540b1beb78d14f0a36a
xlt3h <richatdfreeman@gmail.com>
2022-06-22 11:36:39 +0200

    faster-transmit-live-video_beta

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+'''
+created by ritshidze nemudzivhadi
+github xlt3h
+'''
 
-import socket 
-import utilis 
-import pyautogui
-import struct
-import numpy as np 
+import socket  
 import cv2
-import pickle
+
 class video_server:
     
     def server(self,host,port):
@@ -13,46 +13,58 @@ class video_server:
         self.port = port
         self.s = socket.socket()
         self.s.bind((host,port))
-        self.s.listen(10)
+        self.s.listen(1) #listen to one connection
         self.conn, self.addr = self.s.accept()
     
-    def live_vide(self):
+    def server_recv_video(self):
         while True:
             try:
-                self.record = pyautogui.screenshot()
-                self.frame = np.array(self.record)
-                #send the video via sockets using pickle
-                self.v = pickle.dumps(self.frame)
-                self.video_send = struct.pack("Q", len(self.v)) + self.v
-                self.conn.sendall(self.video_send)
-            except ConnectionResetError:
-                print("connection was lost")
+                self.data = self.conn.recv(2048000)
+                self.img = open("img.jpg","wb")
+                if self.data:
+                    self.img.write(self.data)
+                    self.img = cv2.imread("img.jpg")
+                    cv2.imshow("img",self.img)
+                    if cv2.waitKey(1) == ord('q'):
+                        break
+                else:
+                    print("client connection closed")
+                    break
+            except :
+                print("an error occured")
+                print("restarting the server")
                 #listen again for a new connection
                 self.server(self.host,self.port)
+        #close everything
+        cv2.destroyAllWindows()
+        self.s.close()
+        self.conn.close()
+        #end of server
+
     def client(self,host_c,port_c):
-        self.host_c = host_c
-        self.port_c = port_c
-        self.s_c = socket.socket()
-        self.s_c.connect((self.host_c,self.port_c))
-        self.data = b""
-        self.payload_size = struct.calcsize("Q")
-        while True:
-            while len(self.data) < self.payload_size:
-                self.packet = self.s_c.recv(4*1024)
-                if not self.packet: break
-                self.data += self.packet
-            self.packed_msg_size = self.data[:self.payload_size]
-            self.data = self.data[self.payload_size:]
-            self.msg_size = struct.unpack("Q", self.packed_msg_size)[0]
-            while len(self.data)<self.msg_size:
-                self.data += self.s_c.recv(4*1024)
-            self.frame_data = self.data[:self.msg_size]
-            self.data = self.data[self.msg_size:]
-            self.frame_ = pickle.loads(self.frame_data)
-            cv2.imshow("Live",self.frame_)
-            if cv2.waitKey(1) == ord('q'):
-                break
+        try:
+            self.host_c = host_c
+            self.port_c = port_c
+            self.s_c = socket.socket()
+            self.s_c.connect((self.host_c,self.port_c))
+            self.cap =cv2.VideoCapture(0)
+            while True:
+                self.ret , self.photo= self.cap.read()
+
+                cv2.imwrite("img_1.jpg",self.photo)
+                self.file = open("img_1.jpg","rb")
+                self.img_data = self.file.read(2048000)
+                self.s_c.send(self.img_data)            
+                if cv2.waitKey(1) == ord('q'):
+                    break
+        except:
+            print("an error occured reconnecting...")
+            self.client(self.host_c,self.port_c)
         self.s_c.close()
+        self.cap.release()
+        cv2.destroyAllWindows()
+        self.file.close()
+        #end of client
 
     def record_video(self,filename):
         self.fps = 60.0
@@ -72,7 +84,8 @@ s = video_server()
 t = str(input("Enter 1 for server and 2 for client: "))
 if t == "1":
     s.server("127.0.0.1",4444)
-    s.live_vide()
+    s.server_recv_video()
+   
 elif t == "2":
     s.client("127.0.0.1",4444)
 
